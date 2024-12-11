51c103b4e426c8ba053f355bc07c1b69af2c08f0
Harsh Narwariya <h.narwariya@thesynapses.com>
2024-12-11 12:39:36 +0530

    meaning

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,5 +1,25 @@
+import { useAuthConfig } from "../../api/accounts/queryHooks";
+
+const CheckLogin = ({ children }) => {
+  const { isLoading } = useAuthConfig();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return children;
+};
+
 const Home = () => {
-  return <h2>Home</h2>;
+  const { data } = useAuthConfig();
+
+  return (
+    <CheckLogin>
+      <div className="w-screen h-screen bg-[#16151A]">
+        {data?.user ? data.user.email : "Login Creds Not Found"}
+      </div>
+    </CheckLogin>
+  );
 };
 
 export default Home;
