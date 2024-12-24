c683440c925bd44ae69504ce7384ecedd582e69d
Harsh Narwariya <h.narwariya@thesynapses.com>
2024-12-24 18:05:54 +0530

    added img lazy loading on album card

--- src/components/songcards/albumcard.js
+++ src/components/songcards/albumcard.js
@@ -1,14 +1,40 @@
 import { ALink } from "../../components/links/links";
 import { get_src_uri } from "../../api/utils";
+import { useEffect, useRef } from "react";
 
 const AlbumCard = ({ album }) => {
+  const imgRef = useRef(null);
+  const imgContainerRef = useRef(null);
+
+  useEffect(() => {
+    const imgEle = imgRef.current;
+    const imgContainerEle = imgContainerRef.current;
+
+    const handleLoaded = () => {
+      imgEle.classList.remove("opacity-0");
+      imgContainerEle.classList.remove("skeleton");
+    };
+
+    if (imgEle && imgContainerRef) {
+      if (imgEle.complete) handleLoaded();
+      else imgEle.addEventListener("load", handleLoaded);
+
+      return () => {
+        imgEle.removeEventListener("load", handleLoaded);
+      };
+    }
+  }, [imgRef, imgContainerRef]);
+
   return (
     <div className="group">
       <div
-        className={`relative cursor-pointer flex flex-col items-center justify-center w-full aspect-square rounded-xl overflow-hidden bg-[#000]`}
+        ref={imgContainerRef}
+        className={`skeleton relative cursor-pointer flex flex-col items-center justify-center w-full aspect-square rounded-xl overflow-hidden bg-[#000]`}
       >
         <img
-          className="absolute top-0 left-0 w-full aspect-square"
+          ref={imgRef}
+          loading="lazy"
+          className="absolute top-0 left-0 w-full aspect-square opacity-0 transition-opacity duration-500"
           alt={album.title}
           src={get_src_uri(album.thumbnail300x300)}
         />
