11997db754bbe0c64a00829a16c346f14e5f2bdc
Harsh Narwariya <h.narwariya@thesynapses.com>
2024-12-25 13:49:45 +0530

    seperated

---
+++ src/components/selectors/sortoptions.js
@@ -0,0 +1,49 @@
+import { useEffect, useRef, useState } from "react";
+
+const SortOptions = ({ options }) => {
+  const sliderRef = useRef(null);
+  const firstOptionBtnRef = useRef(null);
+
+  const [option, setOption] = useState(options[0]);
+
+  const handleClick = (e, item) => {
+    const selectedButton = e.target;
+    sliderRef.current.style.transform = `translateX(${selectedButton.offsetLeft}px)`;
+    sliderRef.current.style.width = `${selectedButton.offsetWidth}px`;
+    setOption(item);
+  };
+
+  useEffect(() => {
+    firstOptionBtnRef.current?.click();
+  }, [firstOptionBtnRef]);
+
+  return (
+    <div className="h-10 bg-[#222227] flex items-center rounded-xl relative">
+      <div
+        ref={sliderRef}
+        className="h-[30px] absolute top-[5px] bg-[#25a56a] rounded-[10px] transition-all duration-200"
+      ></div>
+      <button
+        ref={firstOptionBtnRef}
+        onClick={(e) => handleClick(e, options[0])}
+        className={`h-[30px] mx-[5px] rounded-[10px] px-[15px] text-sm relative transition-colors duration-200 ${
+          options[0].id === option?.id ? "text-white" : ""
+        }`}
+      >
+        {options[0].name}
+      </button>
+      {options.slice(1).map((item) => (
+        <button
+          onClick={(e) => handleClick(e, item)}
+          className={`h-[30px] mx-[5px] rounded-[10px] px-[15px] text-sm relative transition-colors duration-200 ${
+            item.id === option?.id ? "text-white" : ""
+          }`}
+        >
+          {item.name}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default SortOptions;
--- src/pages/search/search.js
+++ src/pages/search/search.js
@@ -1,80 +1,5 @@
-import { useEffect, useRef, useState } from "react";
-import { SearchSvg } from "../../assets/svg";
-
-const SortOptions = () => {
-  const sliderRef = useRef(null);
-  const firstOptionBtnRef = useRef(null);
-
-  const options = [
-    {
-      id: 1,
-      name: "Featured",
-    },
-    {
-      id: 2,
-      name: "Popular",
-    },
-    {
-      id: 3,
-      name: "Newest",
-    },
-  ];
-
-  const [option, setOption] = useState(options[0]);
-
-  const handleClick = (e, item) => {
-    const selectedButton = e.target;
-    sliderRef.current.style.transform = `translateX(${selectedButton.offsetLeft}px)`;
-    sliderRef.current.style.width = `${selectedButton.offsetWidth}px`;
-    setOption(item);
-  };
-
-  useEffect(() => {
-    firstOptionBtnRef.current?.click();
-  }, [firstOptionBtnRef]);
-
-  return (
-    <div className="h-10 bg-[#222227] flex items-center rounded-xl relative">
-      <div
-        ref={sliderRef}
-        className="h-[30px] absolute top-[5px] bg-[#25a56a] rounded-[10px] transition-all duration-200"
-      ></div>
-      <button
-        ref={firstOptionBtnRef}
-        onClick={(e) => handleClick(e, options[0])}
-        className={`h-[30px] mx-[5px] rounded-[10px] px-[15px] text-sm relative transition-colors duration-200 ${
-          options[0].id === option?.id ? "text-white" : ""
-        }`}
-      >
-        {options[0].name}
-      </button>
-      {options.slice(1).map((item) => (
-        <button
-          onClick={(e) => handleClick(e, item)}
-          className={`h-[30px] mx-[5px] rounded-[10px] px-[15px] text-sm relative transition-colors duration-200 ${
-            item.id === option?.id ? "text-white" : ""
-          }`}
-        >
-          {item.name}
-        </button>
-      ))}
-    </div>
-  );
-};
-
-const SearchInput = () => {
-  return (
-    <div className="relative w-full">
-      <input
-        className="h-10 w-full bg-[#222227] rounded-xl pl-5 pr-20 focus:outline-none text-white placeholder:text-[#c0c0c0]"
-        placeholder="Search..."
-      ></input>
-      <div className="absolute top-0 right-0 h-full flex items-center mr-5">
-        <SearchSvg className="fill-white w-5 h-5" />
-      </div>
-    </div>
-  );
-};
+import { SearchInput } from "../../components/Inputs/inputs";
+import SortOptions from "../../components/selectors/sortoptions";
 
 const Search = () => {
   document.title = "Search";
@@ -96,7 +21,22 @@ const Search = () => {
               Filters
             </button>
           </div>
-          <SortOptions />
+          <SortOptions
+            options={[
+              {
+                id: 1,
+                name: "Featured",
+              },
+              {
+                id: 2,
+                name: "Popular",
+              },
+              {
+                id: 3,
+                name: "Newest",
+              },
+            ]}
+          />
         </div>
       </section>
     </>
