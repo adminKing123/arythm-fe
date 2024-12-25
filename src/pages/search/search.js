793d8d68de170e115d40c8dbd3524de9f3abdb7e
Harsh Narwariya <h.narwariya@thesynapses.com>
2024-12-25 16:01:33 +0530

    perfect animation

--- src/pages/search/search.js
+++ src/pages/search/search.js
@@ -2,43 +2,46 @@ import { useState } from "react";
 import ReactDOM from "react-dom";
 import { SearchInput } from "../../components/Inputs/inputs";
 import SortOptions from "../../components/selectors/sortoptions";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 
 const SearchModal = ({ isOpen, toggle }) => {
   const handleClose = () => toggle(false);
 
-  if (!isOpen) return null;
-
   return ReactDOM.createPortal(
-    <motion.div
-      className="absolute flex justify-center items-center top-0 left-0 w-screen h-screen bg-[#4f4f4f82] z-20"
-      onClick={handleClose}
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 0.3 }}
-    >
-      <motion.div
-        onClick={(e) => e.stopPropagation()}
-        className="w-full max-w-[680px] min-w-[280px] bg-[#16151A] rounded-xl shadow-2xl"
-        initial={{ scale: 0.8 }}
-        animate={{ scale: 1 }}
-        transition={{ duration: 0.3 }}
-      >
-        <div className="p-[30px] border-b border-[#c0c0c0] flex justify-between items-center">
-          <h3 className="text-xl">Filter and Play Your Music</h3>
-          <button
-            onClick={handleClose}
-            className="text-sm px-2 rounded-lg bg-[#2b2b2b] hover:bg-[#c0c0c0] hover:text-black duration-300"
+    <AnimatePresence>
+      {isOpen && (
+        <motion.div
+          className="absolute flex justify-center items-center top-0 left-0 w-screen h-screen bg-[#4f4f4f82] z-20"
+          onClick={handleClose}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.1 }}
+        >
+          <motion.div
+            onClick={(e) => e.stopPropagation()}
+            className="w-full max-w-[680px] min-w-[280px] bg-[#16151A] rounded-xl shadow-2xl"
+            initial={{ scale: 0.8 }}
+            animate={{ scale: 1 }}
+            exit={{ scale: 0.8 }}
+            transition={{ duration: 0.1 }}
           >
-            Close
-          </button>
-        </div>
-      </motion.div>
-    </motion.div>,
+            <div className="p-[30px] border-b border-[#c0c0c0] flex justify-between items-center">
+              <h3 className="text-xl">Filter and Play Your Music</h3>
+              <button
+                onClick={handleClose}
+                className="text-sm px-2 rounded-lg bg-[#2b2b2b] hover:bg-[#c0c0c0] hover:text-black duration-300"
+              >
+                Close
+              </button>
+            </div>
+          </motion.div>
+        </motion.div>
+      )}
+    </AnimatePresence>,
     document.body
   );
 };
-
 const SearchModalToggler = () => {
   const [open, setOpen] = useState(true);
   return (
