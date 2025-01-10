0efd09ea1b7556944392f05556eacd89c0445600
Harsh Narwariya <h.narwariya@thesynapses.com>
2025-01-10 11:11:40 +0530

    save icon

--- src/pages/queue/queue.js
+++ src/pages/queue/queue.js
@@ -1,4 +1,5 @@
 import { scrollTo } from "../../api/utils";
+import { BookmarkSvg } from "../../assets/svg";
 import Button from "../../components/buttons/buttons";
 import { ALink } from "../../components/links/links";
 import { SongCard3 } from "../../components/songcards/songcard";
@@ -6,19 +7,40 @@ import ROUTES from "../../router/routes";
 import authConfigStore from "../../zstore/authConfigStore";
 import playerStore from "../../zstore/playerStore";
 
-const QueueSongsList = () => {
-  const queue = playerStore((state) => state.queue);
-  const clearQueue = playerStore((state) => state.clearQueue);
-  const removeSong = playerStore((state) => state.removeSong);
+const ShowCurrentPlayingSong = () => {
+  const current_song = playerStore((state) => state.song);
 
-  return queue.length ? (
-    <>
-      <div className="flex justify-between items-center flex-wrap">
-        <h2 className="text-white text-[30px]">Manage Your Song Queue</h2>
+  return <div></div>;
+};
+
+const QueueHeader = () => {
+  const clearQueue = playerStore((state) => state.clearQueue);
+  return (
+    <div className="flex justify-between items-center flex-wrap gap-4">
+      <h2 className="text-white text-[30px]">Manage Your Song Queue</h2>
+      <div className="flex justify-center items-center gap-5">
+        <ALink
+          className={
+            "flex items-center gap-2 bg-[#222227] px-4 py-2 rounded-lg group"
+          }
+        >
+          <BookmarkSvg className="w-4 h-4 fill-white group-hover:fill-[#25a564] transition-colors duration-300" />
+          Save
+        </ALink>
         <ALink onClick={clearQueue} className="text-[16px]">
           Clear Queue
         </ALink>
       </div>
+    </div>
+  );
+};
+
+const QueueList = () => {
+  const queue = playerStore((state) => state.queue);
+  const removeSong = playerStore((state) => state.removeSong);
+
+  return queue.length ? (
+    <>
       <div className="grid gap-[15px] mt-8 grid-cols-1 md:grid-cols-1">
         {queue.map((song, index) => (
           <SongCard3
@@ -44,6 +66,15 @@ const QueueSongsList = () => {
   );
 };
 
+const QueueContainer = () => {
+  return (
+    <>
+      <QueueHeader />
+      <QueueList />
+    </>
+  );
+};
+
 const Queue = () => {
   document.title = "Manage Queue";
   scrollTo("main-content", { top: 0, behavior: "instant" });
@@ -58,7 +89,7 @@ const Queue = () => {
   return (
     <>
       <section className="p-[30px]">
-        <QueueSongsList />
+        <QueueContainer />
       </section>
     </>
   );
